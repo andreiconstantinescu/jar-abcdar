bbfb90f1053b902e904f322cb2ba53f27b6c9375
Dana Cotoran <dana.cotoran@yahoo.com>
2015-01-21 15:43:21 +0200

    finish content work on L1, add screenshoty

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ define(['jquery'], function($) {
     $('.footnote').popover({html: true});
   });
 });
+WebFontConfig = {
+google: { families: [ 'Open+Sans+Condensed:300,700,300italic:latin,latin-ext' ] }
+};
+(function() {
+var wf = document.createElement('script');
+wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
+  //temporarily include font to see what it looks like
